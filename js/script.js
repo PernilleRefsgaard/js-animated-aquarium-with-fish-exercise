2306a32a2984ede69c1b6cfb72cc57a3f6fab281
PernilleRefsgaard <Refsgaardpernille@gmail.com>
2025-04-25 10:21:14 +0200

    javascribt start

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 "use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+    const anchor = document.querySelector(".anchor");
+    const anchorTooltip = document.getElementById("anchor-tooltip");
+    const audio = document.querySelector("audio");
+  });
